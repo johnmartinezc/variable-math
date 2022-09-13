b960da3979add5c4658df2002cb35f06706c1018
johnmartinezc <john.martinez@codeimmersives.com>
2022-09-13 14:27:44 -0400

    update quadratic.js file

--- quadratic.js
+++ quadratic.js
@@ -4,8 +4,13 @@ let c = 6;
 
 let equation = Math.pow(b,2)- (4*a*c)
 
-x1 = -b + Math.sqrt(equation) /(2 * a)
-x2 = -b - Math.sqrt(equation) /(2 * a)
+let divisble = 2 * a
 
-console.log(x1) 
-console.log(x2)
+x1 = (-b / divisble) + (Math.sqrt(equation) /divisble)
+x2 = (-b / divisble) - (Math.sqrt(equation) /divisble)
+
+console.log("X = " + x1) 
+console.log("or")
+console.log("X = " + x2)
+
+
